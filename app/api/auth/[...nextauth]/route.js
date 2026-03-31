8879e0f58b589d087e7cd00571b1488a041837e1
sujal kashivale <kashivalesujal11@gmail.com>
2026-03-31 19:45:08 +0530

    fixed oauth error

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,3 @@
-
-
-
-
 import connectDB from "@/db/connectdb";
 import User from "@/models/User";
 import NextAuth from "next-auth";
@@ -14,6 +10,9 @@ export const { handlers, auth } = NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+
+  secret: process.env.NEXTAUTH_SECRET, // ✅ ADD THIS
+  trustHost: true, // ✅ ADD THIS (important for Vercel)
 
   callbacks: {
     async signIn({ user, account }) {
@@ -43,8 +42,6 @@ export const { handlers, auth } = NextAuth({
         email: session.user.email,
       });
 
-      console.log(dbUser);
-
       if (dbUser) {
         session.user.name = dbUser.username;
       }
@@ -55,6 +52,61 @@ export const { handlers, auth } = NextAuth({
 });
 
 export const { GET, POST } = handlers;
+
+
+// import connectDB from "@/db/connectdb";
+// import User from "@/models/User";
+// import NextAuth from "next-auth";
+// import GitHubProvider from "next-auth/providers/github";
+
+// export const { handlers, auth } = NextAuth({
+//   providers: [
+//     GitHubProvider({
+//       clientId: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//     }),
+//   ],
+
+//   callbacks: {
+//     async signIn({ user, account }) {
+//       if (account.provider === "github") {
+//         await connectDB();
+
+//         let currentUser = await User.findOne({
+//           email: user.email,
+//         });
+
+//         if (!currentUser) {
+//           currentUser = await User.create({
+//             email: user.email,
+//             username: user.email.split("@")[0],
+//           });
+//         }
+
+//         return true;
+//       }
+//       return false;
+//     },
+
+//     async session({ session }) {
+//       await connectDB();
+
+//       const dbUser = await User.findOne({
+//         email: session.user.email,
+//       });
+
+//       console.log(dbUser);
+
+//       if (dbUser) {
+//         session.user.name = dbUser.username;
+//       }
+
+//       return session;
+//     },
+//   },
+// });
+
+// export const { GET, POST } = handlers;
 
 
 
